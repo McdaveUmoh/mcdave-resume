fccef07d6f2ea24fc135e697f8baa9e42f701cba
mcdavri <mcdavecharlesumoh@gmail.com>
2019-05-17 09:38:43 +0100

    initial commit

---
+++ build/precache-manifest.9b2a8a9994d56707fb2ff9868ecc0438.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "041fd2e115d6499f20bd1900d1d85047",
+    "url": "/static/media/study.041fd2e1.png"
+  },
+  {
+    "revision": "22a3b6bf266f10a905bb",
+    "url": "/static/css/main.f45e6235.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a73b7754fa8e57fb2244",
+    "url": "/static/js/2.6e38281e.chunk.js"
+  },
+  {
+    "revision": "60793e98a7baab31a89c00b3129de6d9",
+    "url": "/static/media/mac1.60793e98.jpg"
+  },
+  {
+    "revision": "43a9fc853b8ad634127bf90b5eac0f75",
+    "url": "/static/media/gadget.43a9fc85.png"
+  },
+  {
+    "revision": "e3b14682510f0bac9b92c23c708f0740",
+    "url": "/static/media/curriculum vitae.e3b14682.pdf"
+  },
+  {
+    "revision": "58add10f5a391092d9046174647e32b9",
+    "url": "/static/media/mac5.58add10f.jpg"
+  },
+  {
+    "revision": "982c93d8f09524b561538a29dcb90e71",
+    "url": "/static/media/mcdavename.982c93d8.jpeg"
+  },
+  {
+    "revision": "4e76b269eca43f14f8be96a96957535e",
+    "url": "/static/media/davetechk.4e76b269.png"
+  },
+  {
+    "revision": "b3dfcad20cd86e9040d0382dd8a3b7cc",
+    "url": "/static/media/gadgets.b3dfcad2.png"
+  },
+  {
+    "revision": "31e1163c82f8979d85132d0613ac72b5",
+    "url": "/static/media/wac.31e1163c.png"
+  },
+  {
+    "revision": "3bdc8c03d18a8da0bf9f27ba444b5533",
+    "url": "/static/media/jobtaska.3bdc8c03.png"
+  },
+  {
+    "revision": "80e5e2e08b6f457643d71fb558e1afc1",
+    "url": "/static/media/mines.80e5e2e0.png"
+  },
+  {
+    "revision": "69bddcd872cfa50554dbf0e359a96732",
+    "url": "/static/media/cstemp.69bddcd8.png"
+  },
+  {
+    "revision": "4def8a6d15f036050403da4347e43946",
+    "url": "/static/media/dozie.4def8a6d.png"
+  },
+  {
+    "revision": "1eef2c34ba6570efc4305f7cdb3ebe79",
+    "url": "/static/media/kitchenstuff.1eef2c34.png"
+  },
+  {
+    "revision": "f7ac1b09e586017107456132c6e13ced",
+    "url": "/static/media/urban.f7ac1b09.png"
+  },
+  {
+    "revision": "448b8514de93378ca52937428c938aee",
+    "url": "/static/media/kitchenapp.448b8514.jpeg"
+  },
+  {
+    "revision": "d63b4e2d58d8e2524a9a4364b44db03f",
+    "url": "/static/media/mine.d63b4e2d.jpg"
+  },
+  {
+    "revision": "22a3b6bf266f10a905bb",
+    "url": "/static/js/main.87420fa1.chunk.js"
+  },
+  {
+    "revision": "3e9ff65ac6cbddac5ca9d03808d16617",
+    "url": "/static/media/muni.3e9ff65a.png"
+  },
+  {
+    "revision": "bc042ff776e32119ccb07d93c845dadb",
+    "url": "/static/media/wordalive.bc042ff7.png"
+  },
+  {
+    "revision": "6ab5d5eede442ec23a6219432d427ec4",
+    "url": "/static/media/hama.6ab5d5ee.jpg"
+  },
+  {
+    "revision": "9c8fefe820d6a97b0a4daf53f9bb12a5",
+    "url": "/static/media/allocentric ravels.9c8fefe8.jpeg"
+  },
+  {
+    "revision": "5bc603e905d2d7ee03af24270e7b97ef",
+    "url": "/static/media/allocntricstor.5bc603e9.jpeg"
+  },
+  {
+    "revision": "b77b0f7803717f5e902335342b38ba8b",
+    "url": "/static/media/bob.b77b0f78.jpeg"
+  },
+  {
+    "revision": "a893206502234f603ae48df68b9984f1",
+    "url": "/static/media/bobby.a8932065.jpeg"
+  },
+  {
+    "revision": "b1ebf2299844ced659ea9df449ac38db",
+    "url": "/static/media/akin.b1ebf229.jpeg"
+  },
+  {
+    "revision": "9dc0970924c543c2e5157df9ea67091a",
+    "url": "/static/media/lividgreat.9dc09709.png"
+  },
+  {
+    "revision": "9d38e2d630933cb5d217d30f01fce4b6",
+    "url": "/static/media/livid.9d38e2d6.png"
+  },
+  {
+    "revision": "c3966d17b730802a4adc07260d9e852d",
+    "url": "/static/media/busco.c3966d17.jpeg"
+  },
+  {
+    "revision": "b9c893dbe032ee6eeec7174680a24748",
+    "url": "/static/media/hama1.b9c893db.png"
+  },
+  {
+    "revision": "585181827fe97e947e5bc112f7884803",
+    "url": "/static/media/were.58518182.png"
+  },
+  {
+    "revision": "ae3fe2136069e0a7d7e1f31a7b326765",
+    "url": "/static/media/Jonas1.ae3fe213.png"
+  },
+  {
+    "revision": "9a7467d2a03c0f9d4cb55a6d97b350fd",
+    "url": "/static/media/Allocentric_Travels.9a7467d2.png"
+  },
+  {
+    "revision": "b30b51f4d9cdb17c43d619c2a7e18110",
+    "url": "/static/media/tailorsng.b30b51f4.jpeg"
+  },
+  {
+    "revision": "2117aa6c63a84505db4af5a795eb49b9",
+    "url": "/static/media/mac2.2117aa6c.jpg"
+  },
+  {
+    "revision": "4de10ecec44b4c0b36358707a51705a6",
+    "url": "/static/media/mac6.4de10ece.jpg"
+  },
+  {
+    "revision": "147d34305a3c8ac19f5f6d50cc0da3fb",
+    "url": "/static/media/1.147d3430.png"
+  },
+  {
+    "revision": "7efe45929e0551d9cf71fb793065721b",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Main from "./components/main";
+import {Link} from "react-router-dom";
+import "./App.style.scss";
+import CV from "./components/image/curriculum vitae.pdf";
 
+const FreeLancer = "{Free Lancer}"
 class App extends Component {
-  render() {
+  render() { 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="scroll">
+        {/*Always shows a header, even in smaller screens.*/}
+        <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header ">
+          <header className="mdl-layout__header header-color">
+            <div className="mdl-layout__header-row">
+              {/*Title*/}
+              <Link className="mdl-navigation__link" to="/"><span className="mdl-layout-title">Mcdave Umoh {FreeLancer}</span></Link>
+              {/*Add spacer, to align navigation to the right*/}
+              <div className="mdl-layout-spacer"></div>
+              {/*Navigation. We hide it in small screens.*/}
+              <nav className="mdl-navigation mdl-layout--large-screen-only">
+                <Link className="mdl-navigation__link" to="/aboutme">About Me</Link>
+                <Link className="mdl-navigation__link" to="/contact">Contact</Link>
+                <Link className="mdl-navigation__link" to="/projects">Projects</Link>
+                <Link className="mdl-navigation__link" to="/resume">Resume</Link>
+                <a className="mdl-navigation__link" target="_blank" rel="noopener noreferrer" href={CV}>Download CV</a>
+              </nav>
+            </div>
+          </header>
+          <div className="mdl-layout__drawer header-color">
+          <span className="mdl-layout-title"><Link className="mdl-navigation__link" to="/">Mcdave Umoh</Link></span>
+            <nav className="mdl-navigation">
+              <Link className="mdl-navigation__link" to="/aboutme">About &nbsp; Me</Link>
+              <Link className="mdl-navigation__link" to="/contact">Contact</Link>
+              <Link className="mdl-navigation__link" to="/projects">Projects</Link>
+              <Link className="mdl-navigation__link" to="/resume">Resume</Link>
+              <a className="mdl-navigation__link" target="_blank" rel="noopener noreferrer" href={CV}>Download CV</a>
+            </nav>
+          </div>
+          <main className="mdl-layout__content ">
+            <div className="page-content">{/*Your content goes here*/}
+              <Main/>
+            </div>
+          </main>
+        </div>
       </div>
     );
   }
