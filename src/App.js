aed9f3a88d4c7fa3e441b498067c064b1b3b3b1a
mcdavri <mcdavecharlesumoh@gmail.com>
2019-05-18 05:28:51 +0100

    side bar fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,21 @@ import CV from "./components/image/curriculum vitae.pdf";
 
 const FreeLancer = "{Free Lancer}"
 class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        isOpen: false,
+        menuOpen: true,
+      };
+    }
+
+    hideToggle() {
+      var selectorId = document.querySelector('.mdl-layout');
+      selectorId.MaterialLayout.toggleDrawer();
+  }
+
   render() { 
+    
     return (
       <div className="scroll">
         {/*Always shows a header, even in smaller screens.*/}
@@ -18,7 +32,7 @@ class App extends Component {
               {/*Add spacer, to align navigation to the right*/}
               <div className="mdl-layout-spacer"></div>
               {/*Navigation. We hide it in small screens.*/}
-              <nav className="mdl-navigation mdl-layout--large-screen-only">
+              <nav className="mdl-navigation mdl-layout--large-screen-only" >
                 <Link className="mdl-navigation__link" to="/aboutme">About Me</Link>
                 <Link className="mdl-navigation__link" to="/contact">Contact</Link>
                 <Link className="mdl-navigation__link" to="/projects">Projects</Link>
@@ -27,14 +41,14 @@ class App extends Component {
               </nav>
             </div>
           </header>
-          <div className="mdl-layout__drawer header-color">
-          <span className="mdl-layout-title"><Link className="mdl-navigation__link" to="/">Mcdave Umoh</Link></span>
+          <div className="mdl-layout__drawer header-color" >
+          <span className="mdl-layout-title"><Link className="mdl-navigation__link" onClick={() => this.hideToggle()} to="/">Mcdave Umoh</Link></span>
             <nav className="mdl-navigation">
-              <Link className="mdl-navigation__link" to="/aboutme">About &nbsp; Me</Link>
-              <Link className="mdl-navigation__link" to="/contact">Contact</Link>
-              <Link className="mdl-navigation__link" to="/projects">Projects</Link>
-              <Link className="mdl-navigation__link" to="/resume">Resume</Link>
-              <a className="mdl-navigation__link" target="_blank" rel="noopener noreferrer" href={CV}>Download CV</a>
+              <Link className="mdl-navigation__link" onClick={() => this.hideToggle()} to="/aboutme">About &nbsp; Me</Link>
+              <Link className="mdl-navigation__link" onClick={() => this.hideToggle()}  to="/contact">Contact</Link>
+              <Link className="mdl-navigation__link" onClick={() => this.hideToggle()} to="/projects">Projects</Link>
+              <Link className="mdl-navigation__link" onClick={() => this.hideToggle()} to="/resume">Resume</Link>
+              <a className="mdl-navigation__link" onClick={() => this.hideToggle()} target="_blank" rel="noopener noreferrer" href={CV}>Download CV</a>
             </nav>
           </div>
           <main className="mdl-layout__content ">
